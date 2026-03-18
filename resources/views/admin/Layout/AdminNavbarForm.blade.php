4b7da306ad00af76d3ab5481055924702b405d71
Andravito Brilian Satrya <andravitobs09@gmail.com>
2026-03-18 15:06:08 +0700

    edit secure asset into asset

--- resources/views/admin/Layout/AdminNavbarForm.blade.php
+++ resources/views/admin/Layout/AdminNavbarForm.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', 'Admin Panel')</title>
-    <link rel="stylesheet" href="{{ secure_asset('css/navbar.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('css/form-style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/form-style.css') }}">
 </head>
 
 <body>
@@ -15,6 +15,6 @@
     @yield('content')
 </div>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="{{ secure_asset('js/alert.js') }}"></script>
+<script src="{{ asset('js/alert.js') }}"></script>
 </body>
 </html>
